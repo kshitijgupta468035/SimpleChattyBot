538e477c5d25bc4a30a555f0355f7fe4cb5dba71
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-04 22:57:19 +0530

    create a class SecondTask

---
+++ src/second_task/SecondTask.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: kshitijgupta468035
+ *   Date: 04/04/20
+ *   Time: 22:55 PM
+ */
+
+package second_task;
+
+public class SecondTask {
+}
