5abd9784fae803a7479dcb5f1c60f9898a5efd76
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-04 21:18:55 +0530

    create FirstTaskADT interface for the first_task

---
+++ src/first_task/FirstTaskADT.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: kshitijgupta468035
+ *   Date: 04/04/20
+ *   Time: 21:17 PM
+ */
+
+package first_task;
+
+public interface FirstTaskADT {
+}
