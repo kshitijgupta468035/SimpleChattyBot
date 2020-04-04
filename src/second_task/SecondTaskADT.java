5fbaefde61f54b9641f9df0f759d944098a9934b
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-04 22:42:17 +0530

    create a interface SecondTaskADT

---
+++ src/second_task/SecondTaskADT.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: kshitijgupta468035
+ *   Date: 04/04/20
+ *   Time: 22:41 PM
+ */
+
+package second_task;
+
+public interface SecondTaskADT {
+}
