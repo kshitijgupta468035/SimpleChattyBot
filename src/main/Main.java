eed9b64e0607900d33c8cd26475440da54c6df84
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-05 19:37:15 +0530

    make String variable userName to ask name of user

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,12 @@ package main;
 import java.util.Scanner;
 
 public class Main extends Thread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in); // Creating Scanner class object name scan
+
+        System.out.println("Hello my name is Tom, I am your personal bot." + "\n" + "What is your name?");
+        String userName = scan.nextLine();
+        Thread.sleep(300);
+        System.out.println("Hello!! " + userName + " Nice to meet you.");
     }
 }
