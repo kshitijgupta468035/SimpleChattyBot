a77f4636099342e37c6371e141771ee14dfe53ae
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-05 16:35:05 +0530

    declare a method String createNewFile() with javaDoc comment

--- src/fifth_task/FifthTaskADT.java
+++ src/fifth_task/FifthTaskADT.java
@@ -7,9 +7,18 @@
 
 package fifth_task;
 
+import java.io.IOException;
+
 /**
  * This interface will be implemented by class FifthTask.
  */
 public interface FifthTaskADT {
+    /**
+     * This method will is used to create a new file.
+     *
+     * @return the user choice for next instruction.
+     * @throws IOException if file not created properly.
+     */
+    String createNewFile() throws IOException;
 
 }
