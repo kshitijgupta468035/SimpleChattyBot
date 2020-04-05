96db11a195e06b7e37a390089c6db7d3a6b84dc9
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-05 16:21:35 +0530

    completing the implemented method void guessNumber() and also handles ArrayIndexOfBoundsException

--- src/fourth_task/FourthTask.java
+++ src/fourth_task/FourthTask.java
@@ -40,6 +40,31 @@ public class FourthTask implements FourthTaskADT {
             int countCows = 0;
             int countBulls = 0;
 
+            // catch (ArrayIndexOutOfBoundsException)
+            try {
+                for (int i = 0; i < randomGuessedNumber.length(); i++) {
+                    for (int j = 0; j < randomGuessedNumber.length(); j++) {
+                        if (first[i] == second[j]) {
+                            countCows += 1;
+                        }
+                    }
+                }
+
+                countBulls = randomGuessedNumber.length() - countCows;
+
+                if ((countBulls == 1) && (countCows == 1)) {
+                    System.out.println("\033[1;94m" + "\nYou got " + countCows + " cow and " + countBulls + " bull." + "\033[0m");
+                } else if (countBulls > 1 && countCows > 1) {
+                    System.out.println("\033[1;94m" + "\nYou got " + countCows + " cows and " + countBulls + " bulls." + "\033[0m");
+                } else if (countBulls == 1) {
+                    System.out.println("\033[1;94m" + "\nYou got " + countCows + " cows and " + countBulls + " bull." + "\033[0m");
+                } else if (countCows == 1) {
+                    System.out.println("\033[1;94m" + "\nYou got " + countCows + " cow and " + countBulls + " bulls." + "\033[0m");
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("\033[1;94m" + "\nIncomplete Number or no Number, Write Number again." + "\033[0m");
+            }
         } while (response == true);
     }
 }
