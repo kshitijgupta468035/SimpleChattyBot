dcccfcbfa6af141db0605cc2a537c4d475411ea4
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-05 15:50:30 +0530

    create a interface FourthTaskADT

---
+++ src/fourth_task/FourthTaskADT.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: kshitijgupta468035
+ *   Date: 05/04/20
+ *   Time: 15:49 PM
+ */
+
+package fourth_task;
+
+public interface FourthTaskADT {
+}
