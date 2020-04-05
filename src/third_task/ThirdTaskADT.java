951c3dd897ddbc3e1a7ed91b3f8db4e9fa9d430b
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-04-05 12:09:51 +0530

    create a interface ThirdTaskADT

---
+++ src/third_task/ThirdTaskADT.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: kshitijgupta468035
+ *   Date: 05/04/20
+ *   Time: 12:08 PM
+ */
+
+package third_task;
+
+public interface ThirdTaskADT {
+}
